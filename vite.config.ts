76cbb8c89c9e7517e7a69d7be8dc333585054a3a
Marlon Wanger <marlonsoftwares@hotmail.com>
2024-04-04 22:40:24 -0400

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "redlust-ui",
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  plugins: [react()],
+})
+  
